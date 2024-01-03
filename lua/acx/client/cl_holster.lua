84ef1d85873ade2ccc553ebabac30f156dd0ec54
Haodong Mo <mohaodong@gmail.com>
2024-01-03 23:18:46 +1000

    Holster less delayed

--- lua/acx/client/cl_holster.lua
+++ lua/acx/client/cl_holster.lua
@@ -2,7 +2,7 @@ hook.Add("StartCommand", "ACX_Holster", function(ply, ucmd)
     local wep = ply:GetActiveWeapon()
 
     if IsValid(wep) and wep.ACX then
-        if wep:GetHolsterTime() != 0 and wep:GetHolsterTime() <= CurTime() then
+        if wep:GetHolsterTime() != 0 and wep:GetHolsterTime() - wep:GetPingOffsetScale() <= CurTime() then
             if IsValid(wep:GetHolsterEntity()) then
                 wep:SetHolsterTime(-1)
                 ucmd:SelectWeapon(wep:GetHolsterEntity()) -- Call the final holster request
